d855bd27b3683eee913a00099248e56d766c7bfe
nittics1991 <bunpabcom@gmail.com>
2019-03-29 16:43:21 +0900

    buttonMenue button.optionsに変更

--- ButtonMenu/cmb.js
+++ ButtonMenu/cmb.js
@@ -16,37 +16,70 @@ class ConcertoMb
 				'add','remove','save','reset','upload','reset',
 			],
 			add:{
-				icon:"ui-icon-plusthick",
+				options:{
+					icons:{
+						primary:"ui-icon-plusthick",
+					},
+		  			text: false,
+				},
 				click:function(e) {
-					console.log("AAA");
+					console.log(e);
+					console.log(e.target);
+					console.log(e.target.parentNode);
+					console.log(e.target.parentNode.parentNode);
 				},
 			},
 			remove:{
-				icon:"ui-icon-minusthick",
+				options:{
+					icons:{
+						primary:"ui-icon-minusthick",
+					},
+		  			text: false,
+				},
 				click:function(e) {
 					console.log("BBB");
 				},
 			},
 			save:{
-				icon:"ui-icon-disk",
+				options:{
+					icons:{
+						primary:"ui-icon-disk",
+					},
+		  			text: false,
+				},
 				click:function(e) {
 					console.log("CCC");
 				},
 			},
 			reset:{
-				icon:"ui-icon-home",
+				options:{
+					icons:{
+						primary:"ui-icon-home",
+					},
+		  			text: false,
+				},
 				click:function(e) {
 					console.log("DDD");
 				},
 			},
 			download:{
-				icon:"ui-icon-arrowthickstop-1-s",
+				options:{
+					icons:{
+						primary:"ui-icon-arrowthickstop-1-s",
+					},
+		  			text: false,
+				},
 				click:function(e) {
 					console.log("EEE");
 				},
 			},
 			upload:{
-				icon:"ui-icon-arrowthickstop-1-n",
+				options:{
+					icons:{
+						primary:"ui-icon-arrowthickstop-1-n",
+					},
+		  			text: false,
+				},
 				click:function(e) {
 					console.log("FFF");
 				},
@@ -96,12 +129,10 @@ class ConcertoMb
 		let buttonset = this.el.children[0].children[1];
 		
 		$(buttonset.children).map(function(i, el) {
-			$(el).button({
-	  			text: false,
-	  			icons: {
-	      		  primary:buttons[layout[i]].icon || ''
-		  		}
-	  		}).click(
+			$(el)
+			.button(buttons[layout[i]].options || {})
+	  		.on(
+	  			'click',
 	  			buttons[layout[i]].click
 	  		);
 			
